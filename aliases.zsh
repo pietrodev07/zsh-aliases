4ad561a3e3a3c5d263d8cc4315adcec17429edf0
pietrodev07 <pietro.dev.07@gmail.com>
2024-10-12 18:19:04 +0200

    feat(zsh-aliases): add zsh aliases file

---
+++ aliases.zsh
@@ -0,0 +1,18 @@
+alias code="code-insiders"
+alias code-sw="code-insiders --reuse-window"
+
+alias gcm="git commit -m"
+alias gcl="git clone"
+alias gpl="git pull"
+alias gph="git push"
+alias gad="git add"
+alias gck="git checkout"
+alias gmr="git merge"
+
+alias pm="pnpm"
+alias pma="pnpm add"
+alias pmr="pnpm remove"
+alias pmi="pnpm install"
+
+alias cl="clear"
+alias mk="make"
